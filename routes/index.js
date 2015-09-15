5806425993af2c13614692fd99346128b6d31cd2
valentin.georgiev <valentin.georgiev@trading212.com>
2015-09-15 16:29:12 +0300

    separated html in different files

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'BrowserCad Home' });
+  res.render('index', {
+      title: 'BrowserCad Home' ,
+      partials: {
+          codeOutput: 'codeOutput',
+          operationModal: 'operationModal',
+          geometryModal: 'geometryModal'
+      }
+  });
 });
 
 module.exports = router;
